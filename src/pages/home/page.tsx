e554a6548e8630db571873748dbc69e2787de105
sushantshrestha22 <newasushant.ns16@gmail.com>
2025-08-04 15:56:16 +0545

    feat: resume download added

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { typo } from "@/constant/typography";
 import Photo from "@/assets/images/photo.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -18,15 +19,19 @@ const Home = () => {
           improving my skills with the latest technologies.
         </p>
         <div className="flex flex-col sm:flex-row gap-4">
-          <Button className={`${typo.h5}`}>View Resume</Button>
-          <Button variant="outline" className={`${typo.h5}`}>
-            Contact Me
-          </Button>
+          <a href="@/assets/Resume.pdf" download="Sushant_Shrestha_Resume.pdf">
+            <Button className={`${typo.h5} cursor-pointer`}>View Resume</Button>
+          </a>
+          <Link to="/contact">
+            <Button variant="outline" className={`${typo.h5}`}>
+              Contact Me
+            </Button>
+          </Link>
         </div>
       </section>
       <section className="w-full flex justify-center items-end h-full lg:h-[95vh] relative order-1 lg:order-2 max-lg:border-b border-secondary">
         <div className="absolute inset-0 bg-gradient-to-tr from-accent via-accent to-accent blur-3xl rounded-full aspect-square w-[80vw] max-w-[90%] lg:max-w-[90%] animate-pulse top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0 " />
-      
+
         <img
           src={Photo}
           alt="Sushant Shrestha"
